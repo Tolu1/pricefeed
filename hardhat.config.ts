7c148422ca330fb6afecb1159f126ab49f235a2f
Tolu Olugbesan <tolugbesan@gmail.com>
2024-02-25 15:39:44 +0100

    Fix config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,13 +6,6 @@ const config: HardhatUserConfig = {
   solidity: "0.8.24",
   defaultNetwork: "hardhat",
   networks: {
-    hardhat: {
-      loggingEnabled: false,
-    },
-    //   localhost: {
-    //     url: "http://127.0.0.1:8545",
-    //     accounts: [process.env.HARDHAT_ACCOUNT_PRIVATE_KEY || ""],
-    // },
     "arbitrum-sepolia": {
       url: "https://sepolia-rollup.arbitrum.io/rpc",
       chainId: 421614,
