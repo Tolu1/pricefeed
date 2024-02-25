ab578da7eb6447d2c8b675fe9f46ac39987d596a
Tolu Olugbesan <tolugbesan@gmail.com>
2024-02-25 19:44:54 +0100

    Verify contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,6 +12,24 @@ const config: HardhatUserConfig = {
       accounts: [process.env.DEV_ACCOUNT_PRIVATE_KEY || ""],
     },
   },
+  etherscan: {
+    apiKey: {
+      arbitrumSepolia: process.env.ARBISCAN_API_KEY!,
+    },
+    customChains: [
+      {
+        network: "arbitrumSepolia",
+        chainId: 421614,
+        urls: {
+          apiURL: "https://api-sepolia.arbiscan.io/api",
+          browserURL: "https://sepolia.arbiscan.io/",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: false,
+  },
 };
 
 export default config;
