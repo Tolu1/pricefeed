da7a1336f3a460bb261608c97581cd0b504b11f2
Tolu Olugbesan <tolugbesan@gmail.com>
2024-02-25 19:44:41 +0100

    Fix deployment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,7 +1,8 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const PRICE_FEED_CONTRACT_ADDRESS_ETH_USD = 0xd30e2101a97dcbaebcbc04f14c3f624e67a35165;
+  const PRICE_FEED_CONTRACT_ADDRESS_ETH_USD =
+    "0xd30e2101a97dcbaebcbc04f14c3f624e67a35165";
   const pricefeedConsumer = await ethers.deployContract("PricefeedConsumer", [
     PRICE_FEED_CONTRACT_ADDRESS_ETH_USD,
   ]);
